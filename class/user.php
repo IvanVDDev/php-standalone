51e803d8f30e6f768bafae3663b7932d959178e2
Ivan Vukadinović <ivanv@lgs036.com>
2020-05-14 11:53:30 +0200

    Initial commit

---
+++ class/user.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Util;
+
+class User {
+    public function test()
+    {
+        echo "test";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require "init.php";
+
+use Util\User;
+
+$user = new User;
+$user->test();
